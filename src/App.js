cbc2901bed61fa05b12a44cac85ceb98ace4bd7c
LukasD <ldeconantsesznak@gmail.com>
2019-12-12 08:38:54 -0500

    Added theme

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, createMuiTheme, ThemeProvider  } from '@material-ui/core/styles';
+import Fab from '@material-ui/core/Fab';
+import Icon from '@material-ui/core/Icon';
 import ActivityLog from './components/ActivityLog';
 import { StoreProvider } from "./store";
 import initialState from "./store/initialState";
 
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#5FB9A6',
+      transparent: 'rgba(95, 185, 166, 0.5)'
+    },
+    secondary: {
+      main: '#C68A77'
+    }
+  }
+});
+
 const useStyles = makeStyles(theme => ({
   root: {
     width: '100%',
-    maxWidth: 360,
+    height: '100vh',
     backgroundColor: theme.palette.background.paper,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
   },
 }));
 
 function App() {
+
+  const classes = useStyles();
+
   return (
-    <StoreProvider initialState={initialState}>
-      <div className="App">
-        <ActivityLog />
-      </div>
-    </StoreProvider>
+    <ThemeProvider theme={theme}>
+      <StoreProvider initialState={initialState}>
+        <div className={classes.root}>
+          <ActivityLog />
+          <Fab color="primary" aria-label="add">
+            <Icon>add</Icon>
+          </Fab>
+        </div>
+      </StoreProvider>
+    </ThemeProvider>
   );
 }
 
